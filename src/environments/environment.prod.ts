cc724ccc7f91775bb3050257bcca43e1d455b62d
Denis Levkov <dlevkov@gmail.com>
2018-03-30 15:50:53 +0300

    WIP

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBDAtMLdlRFhnv1UMCvEbxAYUq9ZpqBKAw',
+    authDomain: 'stockticker-cf877.firebaseapp.com',
+    databaseURL: 'https://stockticker-cf877.firebaseio.com',
+    projectId: 'stockticker-cf877',
+    storageBucket: 'stockticker-cf877.appspot.com',
+    messagingSenderId: '79680659123'
+  }
 };
