bc18f7087601f116b8974e8ffd73bbeaf859e049
Denis Levkov <dlevkov@gmail.com>
2018-03-30 11:49:38 +0300

    WIP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,16 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MaterialModule } from './material/material.module';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { PortfolioComponent } from './portfolio/portfolio.component';
 import { MyPortfolioComponent } from './portfolio/my-portfolio/my-portfolio.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 import { StockSelectorComponent } from './portfolio/stock-selector/stock-selector.component';
+
 @NgModule({
   declarations: [AppComponent, PortfolioComponent, MyPortfolioComponent, StockSelectorComponent],
   imports: [
